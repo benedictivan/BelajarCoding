ad087a1a9118c8b1878d3226d54c69be3e671200
Ivan <benedict.ivan10.10@gmail.com>
2023-09-04 23:55:21 +0700

    1st Learn

---
+++ Sololearn/Introduction_to_JS/CodeCoach.js
@@ -0,0 +1,12 @@
+// Test Case #1
+// The given program should output the text : "Solving my first JavaScript Code Coach!" but something is wrong 
+// Code : console. ("Solving my first JavaScript Code Coach!");
+// Answer : 
+console.log ("Solving my first JavaScript Code Coach!");
+
+// Test Case #2
+// Soccer Team -> A Soccer Team "WON" 3 games and ended 1 game with a "DRAW".
+// Win = 3 points, Draw = 1 points
+// Task = Calculate Output the total points earned by the team
+// Answer : 
+console.log ((3*3)+(1*1));
---
+++ Sololearn/Introduction_to_JS/Practices.js
@@ -0,0 +1,26 @@
+// Practices #1 - Delivery Services
+// You are developing a "COOL" websites for delivery service and need to test the homepage, where users are asked to input their addresses. your code is designed to output:
+// Welcome! | code : .log("Welcome!")
+// Enter your address. | code : console.log("Enter your address."
+// Answer :
+console.log("Welcome!");
+console.log("Enter your address.");
+
+// Practices #2 - Everyone can code! 
+// The given code outputs "Coding is for engineers" and "Coding is for everyone".
+// Task : Comment Out part of the code to make it output only "Coding is for everyone".
+// Answer : 
+/*
+console.log("Coding is for engineers");
+*/
+console.log("Coding is for everyone");
+
+// Practices #3 - Simple Operations 
+// How many seconds are there in a month (Assumed 1 Month = 30 days)? 
+// Task : Write a Program to calculate and output the answer
+// Answer : 
+console.log(30*(24*60*60));
+
+
+
+
